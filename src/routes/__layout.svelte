77f4a71184ef3980190e3c9a100cb5ee72e61c49
ad2701ly <shay.ryu@gmail.com>
2022-02-02 12:02:30 +0700

    init putra packaging repo

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class=" w-screen h-screen bg-purple-400">
+  <slot />
+</div>
+
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
